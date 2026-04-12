fc882f8a406d8e2012207585fe062872c32aa8a6
Olle Månsson <31876997+ollema@users.noreply.github.com>
2026-04-12 22:48:23 +0200

    format

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -4,7 +4,7 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async () => {
 	return await response({
-		origin: "https://skiftesgatan.se",
+		origin: 'https://skiftesgatan.se',
 		excludeRoutePatterns: ['^/konto', '^/tvattstuga', '^/uteplats', '^/kalender', '^/api'],
 		paramValues: {
 			'/nyheter/[slug]': allNews.map((n) => n._meta.path),
